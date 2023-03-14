cb00d43c5be2946549c83369eb1fcbdca36d1a4c
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-03-14 12:57:26 -0300

    Minor changes to camera + Gyro Movment Comments

--- Assets/Code/Script/Camera/FollowTarget.cs
+++ Assets/Code/Script/Camera/FollowTarget.cs
@@ -6,31 +6,23 @@ public class FollowTarget : MonoBehaviour
 {
     [Header("Values")]
     [SerializeField] private Transform _target;
+    [SerializeField] private Transform _cameraPosition;
+    [SerializeField] private Transform _cameraRotation;
     [SerializeField] private Vector3 _initialDesiredLocation;
-    private float _targetInitialDistance;
-    private float _meshMagnitude;
 #if UNITY_EDITOR
     [SerializeField] private bool _debugDraw;
     [SerializeField] private Color _color;
 #endif
-    private void Awake()
-    {
-        if (_target)
-        {
-            _targetInitialDistance = Vector3.Distance(_target.position + _initialDesiredLocation, _target.position);
-            _meshMagnitude = _target.GetComponentInChildren<MeshFilter>().mesh.bounds.extents.magnitude;
-        }
-    }
     private void Update()
     {
-        transform.position = _target.position + (_target.localScale.magnitude * _meshMagnitude + _targetInitialDistance) * -transform.forward; 
+        _cameraPosition.position = _target.position + _initialDesiredLocation + _target.localScale.magnitude * -_cameraPosition.forward;
     }
     
 #if UNITY_EDITOR
     public void RepositionCam()
     {
-        transform.position = _target.position + _initialDesiredLocation;
-        transform.LookAt(_target.position, Vector3.up);
+        _cameraPosition.position = _target.position + _initialDesiredLocation;
+        _cameraPosition.LookAt(_target.position, Vector3.up);
     }
     private void OnDrawGizmosSelected()
     {
--- Assets/Code/Script/Controls/Actions/MovmentByGyro.cs
+++ Assets/Code/Script/Controls/Actions/MovmentByGyro.cs
@@ -4,6 +4,7 @@ public class MovmentByGyro : BaseActions
 {
     private Gyroscope gyro;
     private Vector3 _baseGravity;
+    //private Vector3 _currentAngle;
     [SerializeField] private float _minGyroForce;
     [SerializeField] private float _maxAngle;
 
@@ -13,7 +14,7 @@ public class MovmentByGyro : BaseActions
         {
             gyro = Input.gyro;
             gyro.enabled = true;
-            _baseGravity = gyro.gravity;
+            _baseGravity = gyro.gravity;//new Vector3(gyro.gravity.y, 0, -gyro.gravity.x);
 
             SetTarget(transform, GetComponent<Rigidbody>());
         }
@@ -36,20 +37,34 @@ public class MovmentByGyro : BaseActions
     {
         if (gyro != null)
         {
+            // naka
             Vector3 rotation = gyro.gravity - _baseGravity;
-            print(rotation);
             if (rotation.sqrMagnitude > _minGyroForce)
             {
                 rotation = new Vector3(rotation.y, 0, -rotation.x);
-                rotation = Vector3.ClampMagnitude(rotation, 0.4f);
+                rotation = Vector3.ClampMagnitude(rotation, _maxAngle);
                 _rb.AddTorque(rotation * _sensitivity, ForceMode.Acceleration);
+                //Debug.Log((rotation * _sensitivity).magnitude);
             }
+
+            //vini
+            //Vector3 gyroMovment = new Vector3(gyro.rotationRateUnbiased.y, 0, -gyro.rotationRateUnbiased.x);
+            //_currentAngle += gyroMovment;
+            //if (_currentAngle.magnitude >= _minGyroForce)
+            //{
+            //    Vector3 direction = gyro.gravity - _baseGravity;
+            //    direction = new Vector3(direction.y, 0, -direction.x);
+            //    _rb.AddTorque(_currentAngle.magnitude * direction * _sensitivity, ForceMode.Acceleration);
+            //    Debug.Log(total.magnitude);
+            //}
         }
     }
 
     public void ResetGyroPosition()
     {
         _baseGravity = gyro.gravity;
+        //_currentAngle = Vector3.zero;
+        //_rb.velocity = Vector3.zero;
     }
 
 }

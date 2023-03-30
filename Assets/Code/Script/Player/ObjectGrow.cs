e79d2d719c9fdf4cb93ac97931d79ec083cf6ea0
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-03-30 19:15:12 -0300

    Changes to object grow calc + player with UI prefab

--- Assets/Code/Script/Player/ObjectGrow.cs
+++ Assets/Code/Script/Player/ObjectGrow.cs
@@ -13,15 +13,11 @@ public class ObjectGrow : MonoBehaviour
     [SerializeField] private float _minSize;
     [SerializeField] private float _maxSize;
     [SerializeField, Range(0f, 1f), Tooltip("the minimum size to eat something in percentage")] private float _minRequiredSizeToEatPercentage;
-    [SerializeField, Range(0f, 1f), Tooltip("the size increase relative to its initial size")] private float _sizeIncreaseFactor = .1f;
+    [SerializeField, Tooltip("the size increase relative to its initial size")] private float _sizeIncreaseFactor = 1f;
 
     private float _currentSize;
-    private float _sizeIncrease;
-    private Vector3 _initialSize;
     private float _initialMass;
 
-    //public Vector3 InitialSize => _initialSize;
-    //public float InitialMass => _initialMass;
     public Transform ObjectToGrow => _objectToGrow;
     public Rigidbody ObjectPhysics => _objectPhysics;
     public Action OnObjectGrow;
@@ -29,9 +25,7 @@ public class ObjectGrow : MonoBehaviour
     private void Awake()
     {
         _currentSize = _objectToGrow.localScale.magnitude;
-        _initialSize = _objectToGrow.localScale;
         _initialMass = _objectPhysics.mass;
-        _sizeIncrease = _initialSize.x * (1f + _sizeIncreaseFactor);
     }
 
     public void UpdateSize(float objectSize, float objectMass = 0)
@@ -39,6 +33,7 @@ public class ObjectGrow : MonoBehaviour
         //Debug.Log($"current size {_currentSize} , required size {objectSize * _minRequiredSizeToEatPercentage}");
         if (_currentSize >= objectSize * _minRequiredSizeToEatPercentage)
         {
+            float finalVolume = objectSize * .476f;
             if (objectSize < 0)
             {
                 _objectToGrow.localScale = new Vector3(
@@ -47,19 +42,13 @@ public class ObjectGrow : MonoBehaviour
                 Mathf.Clamp(_objectToGrow.localScale.z + objectSize, _minSize, _maxSize));
             }
             else
-            {
-                //increase size relative from the base size of the object that will grow (the GameObject with this script)
+            {                
                 _objectToGrow.localScale = new Vector3(
-                Mathf.Clamp(_objectToGrow.localScale.x + _sizeIncrease, _minSize, _maxSize),
-                Mathf.Clamp(_objectToGrow.localScale.y + _sizeIncrease, _minSize, _maxSize),
-                Mathf.Clamp(_objectToGrow.localScale.z + _sizeIncrease, _minSize, _maxSize));
-                //increases size relative to the object eaten size
-                //Vector3 newSize = new Vector3(
-                //    Mathf.Clamp(_objectToGrow.localScale.x + objectSize, _minSize, _maxSize),
-                //    Mathf.Clamp(_objectToGrow.localScale.y + objectSize, _minSize, _maxSize),
-                //    Mathf.Clamp(_objectToGrow.localScale.z + objectSize, _minSize, _maxSize));
+                Mathf.Clamp(_objectToGrow.localScale.x + finalVolume, _minSize, _maxSize),
+                Mathf.Clamp(_objectToGrow.localScale.y + finalVolume, _minSize, _maxSize),
+                Mathf.Clamp(_objectToGrow.localScale.z + finalVolume, _minSize, _maxSize));
             }
-            if (_objectPhysics.mass + objectMass > _initialMass) _objectPhysics.mass += objectMass;
+            if (_objectPhysics.mass + objectMass > _initialMass) _objectPhysics.mass += finalVolume * _sizeIncreaseFactor;
             else _objectPhysics.mass = _initialMass;
             _currentSize = _objectToGrow.localScale.magnitude;
             OnObjectGrow?.Invoke();
--- Assets/Code/Script/Props/EatibleProp.cs
+++ Assets/Code/Script/Props/EatibleProp.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class EatibleProp : MonoBehaviour
 {
     [SerializeField] private float _massIncrease;
-    //private MeshFilter _meshFilter;
-    //private void Awake()
-    //{
-    //    _meshFilter = GetComponent<MeshFilter>();        
-    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //collision.transform.GetComponent<ObjectGrow>().UpdateSize(_meshFilter.mesh.bounds.extents.magnitude * transform.localScale.magnitude, _massIncrease);
-            other.GetComponentInParent<ObjectGrow>().UpdateSize(transform.localScale.magnitude, _massIncrease);
+            other.GetComponentInParent<ObjectGrow>().UpdateSize(transform.localScale.x * transform.localScale.y * transform.localScale.z, _massIncrease);
             Destroy(gameObject);
         }
     }

be4e72333aedd5c0980a2bacbf7f9298c11baece
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-04-18 09:49:09 -0300

    fix to camera rotation speed + area

--- Assets/Code/Script/Camera/RotateByTouch.cs
+++ Assets/Code/Script/Camera/RotateByTouch.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class RotateByTouch : MonoBehaviour
 {
     [SerializeField] private float _dragTresHold;
-    [SerializeField, Range(0f, 1f)] private float _sensitivity;
+    [SerializeField] private float _sensitivity;
     [SerializeField] private Transform _currentTarget;
     [SerializeField, Range(0f, 1f)] private float _dragAreaPrecentX;
     [SerializeField, Range(0f, 1f)] private float _dragAreaPrecentY;
-#if UNITY_EDITOR
-    [SerializeField, Tooltip("the pivot needs to be at the middle center for better correct calc")] private RectTransform _debugPanel;
-#endif
+    [SerializeField] private RectTransform _area;
 
     private Touch _input;
     private bool _isRotating;
@@ -26,14 +24,14 @@ public class RotateByTouch : MonoBehaviour
 
             if (_input.phase == TouchPhase.Moved && _input.deltaPosition.magnitude > _dragTresHold && CheckTouchArea())
             {
-                _currentTarget.eulerAngles += Screen.currentResolution.width * _sensitivity / Screen.currentResolution.width * new Vector3(0, _input.deltaPosition.x, 0);
+                _currentTarget.eulerAngles += _sensitivity * Time.deltaTime * new Vector3(0, _input.deltaPosition.x, 0);
             }
         }
     }
 
     private bool CheckTouchArea()
     {
-        _isRotating = _input.position.y <= Screen.currentResolution.width * _dragAreaPrecentY && _input.position.x < Screen.currentResolution.height * ((1 - _dragAreaPrecentX) / 2 + _dragAreaPrecentX) && _input.position.x > Screen.currentResolution.height * (1 - _dragAreaPrecentX) / 2;
+        _isRotating = _area.rect.Contains(_input.position);
         return _isRotating;
     }
 
@@ -41,8 +39,8 @@ public class RotateByTouch : MonoBehaviour
     [ContextMenu("RecalculateDebugArea")]
     private void RecalcDebugArea()
     {
-        _debugPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.currentResolution.height * _dragAreaPrecentX);
-        _debugPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.currentResolution.width * _dragAreaPrecentY);
+        _area.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Screen.currentResolution.height * _dragAreaPrecentX);
+        _area.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.currentResolution.width * _dragAreaPrecentY);
     }
 #endif
 }

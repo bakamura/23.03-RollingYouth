0e5ebba4b75563c5de954e9b1ecbe3681f52451e
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-05-24 16:25:08 -0300

    ammends for build

--- Assets/Code/Script/Utility/EatiblePropsHelperTool.cs
+++ Assets/Code/Script/Utility/EatiblePropsHelperTool.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -51,3 +52,4 @@ public class EatiblePropsHelperTool : MonoBehaviour
         }
     }
 }
+#endif
